717cf74133f3bb4f6d59a1bbd1ff17a465736384
DhivyaDharshiniT <dhivyadd2912@gmail.com>
2024-01-21 21:19:31 +0530

    day3

---
+++ variable.js
@@ -0,0 +1,10 @@
+a='a'
+var b='var'
+console.log("This variable belong to var type")
+b=10
+console.log("This variable belong to var type",b)
+let c='letter'
+console.log("This variable belog to let type",c)
+const d=126
+console.log("This variable belong to const type",d)
+d=12
